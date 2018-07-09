08872b370a1f208cf667fef6fa50bfba3a9ec248
Nicholas Powers <njpowers7915@gmail.com>
2018-07-09 15:34:47 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -12,7 +12,7 @@ class Song < ActiveRecord::Base
 
   def artist_name=(name)
       self.artist = Artist.find_or_create_by(name: name)
-      binding.pry
+      #binding.pry
       #if self.artist == nil
       #self.artist.name = name
       #end
