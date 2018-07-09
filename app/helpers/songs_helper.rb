e78e9146cef57dae5144ff94264defa1e7654525
Nicholas Powers <njpowers7915@gmail.com>
2018-07-09 14:52:32 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -10,7 +10,7 @@ module SongsHelper
     end
 
     def display_artist(song)
-        if artist_name(song) != nil && != ''
+        if artist_name(song) != nil && != ""
             link_to artist_name(song), artist_path(song.artist)
         else
             link_to "Add Artist", edit_song_path(song)
